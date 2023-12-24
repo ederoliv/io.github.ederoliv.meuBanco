a5f42d60e0333adf437fb683877f76672d0040ab
ederoliv <ederoliveira152@gmail.com>
2023-12-24 03:48:20 -0300

    Changes....

--- src/Principal.java
+++ src/Principal.java
@@ -2,11 +2,12 @@ public class Principal {
 
     public static void main (String[] args){
 
-
-        Conta conta1 = new Conta();
         Pessoa titular1 = new Pessoa();
-        Conta conta2 = new Conta();
         Pessoa titular2 = new Pessoa();
+
+        Conta conta1 = new Conta(titular1, 1, 100);
+        Conta conta2 = new Conta(titular2, 1, 101);
+
 
         titular1.nome = "Eder Oliveira";
         titular1.documento = "01920542019";
@@ -15,14 +16,16 @@ public class Principal {
         titular2.documento = "02020642120";
 
         conta1.titular = titular1;
-        conta1.agencia = 1;
-        conta1.numero = 100;
-        conta1.saldo = 15_000;
+
+
+        conta1.depositar(15_000);
 
         conta2.titular = titular2;
-        conta2.agencia = 1;
-        conta2.numero = 101;
-        conta2.saldo = 20_000;
+
+        conta2.depositar(20_000);
+
+        conta1.sacar(10_000, 5);
+        conta2.sacar(16_000);
 
         System.out.println(conta1.titular.nome);
         System.out.println(conta1.saldo);
